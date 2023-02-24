f2810b6eed302bfec370d36a97e668451099de27
Plush777 <sky11916@naver.com>
2023-02-24 20:38:26 +0900

    test

--- src/components/article/FootSupport.jsx
+++ src/components/article/FootSupport.jsx
@@ -3,7 +3,7 @@ import * as FootBarstyled from 'components/style/common/FootBar.style'
 const FootSupport = () => {
     return ( 
         <FootBarstyled.Bar backgroundColor="#1C559C">
-            <FootBarstyled.Txt href="https://map64wlb3a2.typeform.com/to/vGp6k1Nc" target="_blank" rel="noopener noreferrer"><strong>KartRider Tips Beta</strong>피드백 남기기</FootBarstyled.Txt>
+            <FootBarstyled.Txt href="/https://map64wlb3a2.typeform.com/to/vGp6k1Nc" target="_blank" rel="noopener noreferrer"><strong>KartRider Tips Beta</strong>피드백 남기기</FootBarstyled.Txt>
         </FootBarstyled.Bar>
     );
 }
