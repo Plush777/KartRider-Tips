ba51f0a1d0af162b41eb34bd8681e9ebeaa3b114
Plush777 <sky11916@naver.com>
2023-03-04 22:26:28 +0900

    select 작업중

--- src/components/mobile/Select.jsx
+++ src/components/mobile/Select.jsx
@@ -8,9 +8,16 @@ import { setRouterScroll } from 'redux/store/store';
 const Select = () => {
 
     let [toggle,setToggle] = useState(false);
-    let [selected,setSelected] = useState(null);
-    let [selectState,setSelectState] = useState(null);
-    let getSelectedValue = localStorage.getItem('selected');
+    let [selected,setSelected] = useState(
+        {kart: null},
+        {character: null}
+    );
+    let [selectState,setSelectState] = useState(
+        {kart: null},
+        {character: null}
+    )
+    let getSelectedKartValue = localStorage.getItem('selectedKartbody');
+    let getSelectedCharacterValue = localStorage.getItem('selectedCharacter');
     let dispatch = useDispatch();
     const { pathname } = useLocation();
     let commonKartbodyDesc = useSelector(state => state.kartbodyCommon);
@@ -18,24 +25,37 @@ const Select = () => {
 
     useLayoutEffect(() => {
         if(pathname.startsWith('/kartbody')){
-            setSelectState(commonKartbodyDesc);
+            setSelectState({kart: commonKartbodyDesc})
         } else if(pathname.startsWith('/character')){
-            setSelectState(commonCharacterName);
+            setSelectState({character: commonCharacterName});
         }
     },[pathname,commonKartbodyDesc,commonCharacterName])
 
     useLayoutEffect(() => {
-        if(getSelectedValue === null){
-            setSelected('프로토');
+        if(getSelectedKartValue === null){
+            setSelected({kart: '프로토'});
         } else {
-            setSelected(getSelectedValue);
+            setSelected({kart: getSelectedKartValue});
         }
-    },[getSelectedValue]);
-   
+    },[getSelectedKartValue])
+
+    useLayoutEffect(() => {
+        if(getSelectedCharacterValue === null){
+            setSelected({character: '배찌'});
+        } else {
+            setSelected({character: getSelectedCharacterValue});
+        }
+    },[getSelectedCharacterValue])
 
     const handleClick = e => {
-        setSelected(e.target.textContent);
-        localStorage.setItem('selected',e.target.textContent);
+        setSelected(
+            {kart: e.target.textContent},
+            {character: e.target.textContent}
+        )
+        localStorage.setItem(
+            'selectedKartbody',e.target.textContent,
+            'selectedCharacter',e.target.textContent
+        )
         dispatch(setRouterScroll(false));
         setToggle(false);
     }
@@ -47,17 +67,36 @@ const Select = () => {
     return ( 
         <Selectstyled.SelectArea>
             <Selectstyled.Select onClick={handleToggleSelect} aria-expanded={toggle}>
-                <Selectstyled.SelectTxt>{getSelectedValue === null ? selected : getSelectedValue}</Selectstyled.SelectTxt>
+                {pathname.startsWith('/kartbody') &&
+                    <Selectstyled.SelectTxt>{getSelectedKartValue === null ? selected.kart : getSelectedKartValue}</Selectstyled.SelectTxt>
+                }
+                {pathname.startsWith('/character') &&
+                    <Selectstyled.SelectTxt>{getSelectedCharacterValue === null ? selected.character : getSelectedCharacterValue}</Selectstyled.SelectTxt>
+                }
                 <SCarrowDown width="20px" height="20px" fill="#333"/>
             </Selectstyled.Select>
             <Selectstyled.OptionList className="scY" maxHeight="150px" show={toggle}>
-                {selectState.map((item, index) => {
-                    return(
-                        <Selectstyled.OptionItem key={index}>
-                            <Selectstyled.OptionTxt to={`/kartbody/common/${item.id}`} onClick={handleClick}>{item.title}</Selectstyled.OptionTxt>
-                        </Selectstyled.OptionItem>
-                    )
-                })}
+                {
+                    selectState.character ?
+                    commonCharacterName.map((item,index) => {
+                        return (
+                            <Selectstyled.OptionItem key={index}>
+                                <Selectstyled.OptionTxt as="span" onClick={handleClick}>{item.name}</Selectstyled.OptionTxt>
+                            </Selectstyled.OptionItem>
+                        )
+                    }) :
+                    
+                     selectState.kart ?
+                        commonKartbodyDesc.map((item,index) => {
+                            return (
+                                <Selectstyled.OptionItem key={index}>
+                                    <Selectstyled.OptionTxt to={`/kartbody/common/${item.id}`} onClick={handleClick}>{item.title}</Selectstyled.OptionTxt>
+                                </Selectstyled.OptionItem>
+                            )
+                        }) 
+                        
+                    : null
+                }
             </Selectstyled.OptionList>
         </Selectstyled.SelectArea>
     );
